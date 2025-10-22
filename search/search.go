fbe4fcbc115d43c70a1e5b2e15d9de70649f621d
abdullah-fikri <abdullahfikri981@gmail.com>
2025-10-22 09:22:46 +0700

    fix: return output with slice

--- search/search.go
+++ search/search.go
@@ -8,12 +8,15 @@ func SearchPerson(names []string, keyword string) {
 	found := false
 	for i := range names {
 		if keyword == names[i] {
+			var yes []string
+			yes = append(yes, keyword)
 			found = true
-			fmt.Println("nama ditemukan:", keyword)
+			fmt.Println("nama ditemukan:", yes)
 		}
 	}
+	var notFound []string
 	if !found {
-		fmt.Println("nama tidak ditemukan:", keyword)
+		fmt.Println("nama tidak ditemukan:", notFound)
 	}
 }
 
