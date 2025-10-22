c8d73e9cd103d10688f08a7b79fb703c81b5d69a
abdullah-fikri <abdullahfikri981@gmail.com>
2025-10-22 11:34:45 +0700

    feat: added validation input with defer panic and recover

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golang3/search"
+	"os"
+	"strings"
 )
 
-
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("input salah:", r)
+			os.Exit(1)
+		}
+	}()
+
 	names := []string{"fiki", "yoga", "anggi", "itsna", "federus", "ari", "sidik"}
-	var keyword string
+	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print("Masukkan nama: ")
-	fmt.Scan(&keyword)
+	keyword, _ := reader.ReadString('\n') 
+	keyword = strings.TrimSpace(keyword)  
 
 	search.SearchPerson(names, keyword)
 }
--- search/search.go
+++ search/search.go
@@ -2,21 +2,26 @@ package search
 
 import (
 	"fmt"
+	"strings"
 )
 
 func SearchPerson(names []string, keyword string) {
+	if strings.TrimSpace(keyword) == "" {
+		panic("input kosong!")
+	}
+
 	found := false
+	var yes []string
 	for i := range names {
 		if keyword == names[i] {
-			var yes []string
 			yes = append(yes, keyword)
 			found = true
 			fmt.Println("nama ditemukan:", yes)
 		}
 	}
+
 	var notFound []string
 	if !found {
 		fmt.Println("nama tidak ditemukan:", notFound)
 	}
 }
-
